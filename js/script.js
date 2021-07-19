a34654997b4aaaf230d83a42c1698890c4f5aef3
jadeve <susan.jadeve@gmail.com>
2021-07-19 10:01:17 +0300

    fixing errors

--- js/script.js
+++ js/script.js
@@ -27,12 +27,11 @@ function calculateDay(){
     dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
     console.log(dayOfWeek); //Test the calculateDay function
     return (Math.floor(dayOfWeek));
-    if (dayOfWeek < 0) {
-      dayOfWeek = dayOfWeek * -1;
+    if (dayOfWeek < 0){
+    return dayOfWeek = dayOfWeek * -1;
     }
-    else if (dayOfWeek > 0) {
-      return dayOfWeek;
-    }
+else if (dayOfWeek > 0) {
+  return dayOfWeek;
 }
 
 //main caller func
